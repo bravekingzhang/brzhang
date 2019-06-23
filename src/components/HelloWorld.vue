5e04a2dceba2e7570d1b33d41be4b2766ca999ed
brzhang <hoollyzhang@tencent.com>
2019-06-23 12:28:35 +0800

    【新增】超级牛逼的游戏

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,193 +1,335 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <h3>工具集</h3>
-        <ul>
-            <li><a href="https://www.yuque.com/ruanyf/share/free-diagram"
-                   target="_blank" rel="noopener">免费作图工具
-            </a></li>
-            <li><a href="https://www.explainshell.com/"
-                   target="_blank" rel="noopener">shell命令解释器，碉堡
-            </a></li>
-            <li><a href="https://devhints.io/"
-                   target="_blank" rel="noopener">cheatSheet汇总
-            </a></li>
-            <li><a href="http://frontendtools.com/"
-                   target="_blank" rel="noopener">前端开发工具精选
-            </a></li>
-            <li><a href="https://miku.tools/"
-                   target="_blank" rel="noopener">MikuTools - 工具集合
-            </a></li>
-            <li><a href="https://github.com/my8100/files/tree/master/scrapydweb"
-                   target="_blank" rel="noopener">开源可视化爬虫
-            </a></li>
-            <li><a href="https://github.com/jlevy/the-art-of-command-line/blob/master/README-zh.md"
-                   target="_blank" rel="noopener">bash中的哪些奇技淫巧
-            </a></li>
-            <li><a href="http://tools.jb51.net/regex/create_reg"
-                   target="_blank" rel="noopener">在线正则-包含一些常用
-            </a></li>
-            <li><a href="https://chanind.github.io/hanzi-writer/"
-                   target="_blank" rel="noopener">手写汉字js库
-            </a></li>
-            <li><a href="https://github.com/GitHubDaily/GitHubDaily"
-                   target="_blank" rel="noopener">GitHubDaily
-            </a></li>
-            <li><a href="https://asciinema.org/"
-                   target="_blank" rel="noopener">录制命名行敲下的代码
-            </a></li>
-            <li><a href="https://github.com/nbedos/termtosvg"
-                   target="_blank" rel="noopener">把你敲的命令录制成svg
-            </a></li>
-            <li><a href="https://wangchujiang.com/linux-command/"
-                   target="_blank" rel="noopener">linux命名搜索引擎-带用法
-            </a></li>
-            <li><a href="https://anvaka.github.io/vs/?query=%E5%9C%B0%E7%90%83"
-                   target="_blank" rel="noopener">元搜索
-            </a></li>
-            <li><a href="http://webassembly.org.cn/"
-                   target="_blank" rel="noopener">webassembly
-            </a></li>
-        </ul>
-        <h3>学海无涯苦做舟</h3>
-        <ul>
-            <li><a href="https://kindle.51nazhun.pub/"
-                   target="_blank" rel="noopener">Kindle电子书分享网
-            </a></li>
-            <li><a href="https://github.com/alaskasquirrel/Chinese-Podcasts"
-                   target="_blank" rel="noopener">中文播客
-            </a></li>
-            <li><a href="https://benlauwens.github.io/ThinkJulia.jl/latest/book.html"
-                   target="_blank" rel="noopener">Julia
-            </a></li>
-            <li><a href="http://bob.cs.sonoma.edu/IntroCompOrg-x64/book.html"
-                   target="_blank" rel="noopener">计算机系统导论
-            </a></li>
-            <li><a href="http://craftinginterpreters.com/"
-                   target="_blank" rel="noopener">如何写一个编译器
-            </a></li>
-            <li><a href="https://ksco.gitbooks.io/build-your-own-lisp/"
-                   target="_blank" rel="noopener">用c写一个你自己的lisp
-            </a></li>
-            <li><a href="https://landgreen.github.io/physics/index.html"
-                   target="_blank" rel="noopener">高中物理笔记
-            </a></li>
-            <li><a href="https://love2.io/"
-                   target="_blank" rel="noopener">开源书籍
-            </a></li>
-            <li><a href="https://www.yuque.com/airing/canvas/01"
-                   target="_blank" rel="noopener">html5-canvas教程
-            </a></li>
-        </ul>
-        <h3>Android相关</h3>
-        <ul>
-            <li><a href="https://androidweekly.io/" target="_blank" rel="noopener">Android 开发技术周报</a></li>
-            <li><a href="https://android-developers.googleblog.com/" target="_blank" rel="noopener">Android官方技术博客</a></li>
-            <li><a href="https://zhuanlan.zhihu.com/weishu" target="_blank" rel="noopener">维术不多的Android技巧</a></li>
-        </ul>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>工具集</h3>
+    <ul>
+      <li>
+        <a
+          href="https://www.yuque.com/ruanyf/share/free-diagram"
+          target="_blank"
+          rel="noopener"
+        >免费作图工具</a>
+      </li>
+      <li>
+        <a href="https://www.explainshell.com/" target="_blank" rel="noopener">shell命令解释器，碉堡</a>
+      </li>
+      <li>
+        <a href="https://devhints.io/" target="_blank" rel="noopener">cheatSheet汇总</a>
+      </li>
+      <li>
+        <a href="http://frontendtools.com/" target="_blank" rel="noopener">前端开发工具精选</a>
+      </li>
+      <li>
+        <a href="https://miku.tools/" target="_blank" rel="noopener">MikuTools - 工具集合</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/my8100/files/tree/master/scrapydweb"
+          target="_blank"
+          rel="noopener"
+        >开源可视化爬虫</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/jlevy/the-art-of-command-line/blob/master/README-zh.md"
+          target="_blank"
+          rel="noopener"
+        >bash中的哪些奇技淫巧</a>
+      </li>
+      <li>
+        <a href="http://tools.jb51.net/regex/create_reg" target="_blank" rel="noopener">在线正则-包含一些常用</a>
+      </li>
+      <li>
+        <a href="https://chanind.github.io/hanzi-writer/" target="_blank" rel="noopener">手写汉字js库</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/GitHubDaily/GitHubDaily"
+          target="_blank"
+          rel="noopener"
+        >GitHubDaily</a>
+      </li>
+      <li>
+        <a href="https://asciinema.org/" target="_blank" rel="noopener">录制命名行敲下的代码</a>
+      </li>
+      <li>
+        <a href="https://github.com/nbedos/termtosvg" target="_blank" rel="noopener">把你敲的命令录制成svg</a>
+      </li>
+      <li>
+        <a
+          href="https://wangchujiang.com/linux-command/"
+          target="_blank"
+          rel="noopener"
+        >linux命名搜索引擎-带用法</a>
+      </li>
+      <li>
+        <a
+          href="https://anvaka.github.io/vs/?query=%E5%9C%B0%E7%90%83"
+          target="_blank"
+          rel="noopener"
+        >元搜索</a>
+      </li>
+      <li>
+        <a href="http://webassembly.org.cn/" target="_blank" rel="noopener">webassembly</a>
+      </li>
+    </ul>
+    <h3>学海无涯苦做舟</h3>
+    <ul>
+      <li>
+        <a href="https://kindle.51nazhun.pub/" target="_blank" rel="noopener">Kindle电子书分享网</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/alaskasquirrel/Chinese-Podcasts"
+          target="_blank"
+          rel="noopener"
+        >中文播客</a>
+      </li>
+      <li>
+        <a
+          href="https://benlauwens.github.io/ThinkJulia.jl/latest/book.html"
+          target="_blank"
+          rel="noopener"
+        >Julia</a>
+      </li>
+      <li>
+        <a
+          href="http://bob.cs.sonoma.edu/IntroCompOrg-x64/book.html"
+          target="_blank"
+          rel="noopener"
+        >计算机系统导论</a>
+      </li>
+      <li>
+        <a href="http://craftinginterpreters.com/" target="_blank" rel="noopener">如何写一个编译器</a>
+      </li>
+      <li>
+        <a
+          href="https://ksco.gitbooks.io/build-your-own-lisp/"
+          target="_blank"
+          rel="noopener"
+        >用c写一个你自己的lisp</a>
+      </li>
+      <li>
+        <a
+          href="https://landgreen.github.io/physics/index.html"
+          target="_blank"
+          rel="noopener"
+        >高中物理笔记</a>
+      </li>
+      <li>
+        <a href="https://love2.io/" target="_blank" rel="noopener">开源书籍</a>
+      </li>
+      <li>
+        <a
+          href="https://www.yuque.com/airing/canvas/01"
+          target="_blank"
+          rel="noopener"
+        >html5-canvas教程</a>
+      </li>
+    </ul>
+    <h3>Android相关</h3>
+    <ul>
+      <li>
+        <a href="https://androidweekly.io/" target="_blank" rel="noopener">Android 开发技术周报</a>
+      </li>
+      <li>
+        <a
+          href="https://android-developers.googleblog.com/"
+          target="_blank"
+          rel="noopener"
+        >Android官方技术博客</a>
+      </li>
+      <li>
+        <a href="https://zhuanlan.zhihu.com/weishu" target="_blank" rel="noopener">维术不多的Android技巧</a>
+      </li>
+    </ul>
 
-        <h3>小程序相关</h3>
-        <ul>
-            <li><a href="https://mp.weixin.qq.com/cgi-bin/wx" target="_blank" rel="noopener">官网</a>
-            </li>
-            <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-            <li><a href="https://uniapp.dcloud.io/" target="_blank"
-                   rel="noopener">uniapp</a></li>
-            <li><a href="http://mpvue.com/" target="_blank" rel="noopener">mpvue</a>
-            </li>
-        </ul>
-        <h3>人工智能</h3>
-        <ul>
-            <li><a href="https://github.com/machinelearningmindset/TensorFlow-Course" target="_blank" rel="noopener">TensorFlow新手教程</a>
-            </li>
-            <li><a href="https://developers.google.com/machine-learning/crash-course/" target="_blank" rel="noopener">Google机器学习课程</a>
-            </li>
-        </ul>
-        <h3>娱乐&游戏</h3>
-        <ul>
-            <li><a href="https://arcade.makecode.com/#" target="_blank" rel="noopener">来把，做一个游戏</a>
-            </li>
-            <li><a href="http://tools.jb51.net/games/gangqin" target="_blank" rel="noopener">在线钢琴flash</a>
-            </li>
-            <li><a href="https://github.com/WarpPrism/AutoPiano" target="_blank" rel="noopener">在线钢琴h5</a>
-            </li>
-            <li><a href="http://loderunnerwebgame.com/game/" target="_blank" rel="noopener">挖金子</a>
-            </li>
-            <li><a href="https://zhuanlan.zhihu.com/p/23936161" target="_blank" rel="noopener">非一般的游戏</a>
-            </li>
-            <li><a href="https://dos.zczc.cz/" target="_blank" rel="noopener">DOS游戏-非常经典</a>
-            </li>
-            <li><a href="https://www.foldnfly.com/#/1-1-1-1-1-1-1-1-2" target="_blank" rel="noopener">来教你折纸飞机</a>
-            </li>
-            <li><a href="https://luden.io/wtl/" target="_blank" rel="noopener">人工智能游戏</a>
-            </li>
-        </ul>
-        <h3>知名博主</h3>
-        <ul>
-            <li><a href="http://www.ruanyifeng.com/home.html" target="_blank" rel="noopener">ruanyifeng</a>
-            </li>
-            <li><a href="https://github.com/sdmg15/Best-websites-a-programmer-should-visit" target="_blank" rel="noopener">programer</a>
-            </li>
-        </ul>
-        <h3>比较有意思的一些文章</h3>
-        <ul>
-            <li><a href="https://juejin.im/entry/587e09201b69e600584cd326#foundation" target="_blank" rel="noopener">码农必须书籍</a>
-            </li>
-            <li><a href="https://learn.sparkfun.com/tutorials/how-to-read-a-schematic/all" target="_blank" rel="noopener">电路图符号解释</a>
-            </li>
-            <li><a href="http://blog.coderhuo.tech/2017/10/12/Virtual_Memory_C_strings_proc/" target="_blank" rel="noopener">虚拟内存最佳讲解</a>
-            </li>
-            <li><a href="https://www.forbes.com/sites/maddieberg/2019/02/19/the-greatest-investor-youve-never-heard-of-an-optometrist-who-beat-the-odds-to-become-a-billionaire/#753dab3a22e8" target="_blank" rel="noopener">阅读专利投资得21亿</a>
-            </li>
-            <li><a href="https://github.com/MRNIU/SimpleKernel" target="_blank" rel="noopener">可以玩玩操作系统</a>
-            </li>
-            <li><a href="https://github.com/cfenollosa/os-tutorial" target="_blank" rel="noopener">从零玩操作系统</a>
-            </li>
-            <li><a href="https://hadrienj.github.io/posts/Deep-Learning-Book-Series-Introduction/" target="_blank" rel="noopener">线性代数入门</a>
-            </li>
-            <li><a href="https://hackernoon.com/writing-a-reverse-proxy-in-just-one-line-with-go-c1edfa78c84b" target="_blank" rel="noopener">用go手撸一个反向代理服务器</a>
-            </li>
-            <li><a href="https://www.interviewcake.com/data-structures-reference" target="_blank" rel="noopener">常见数据结构简介</a>
-            </li>
-        </ul>
-        <h3>烧脑</h3>
-        <ul>
-            <li><a href="https://github.com/azl397985856/leetcode" target="_blank" rel="noopener">leetcode解题思路</a>
-            </li>
-            <li><a href="https://github.com/MisterBooo/LeetCodeAnimation" target="_blank" rel="noopener">leetcode解题动画</a>
-            </li>
-            <li><a href="https://algorithm-visualizer.org" target="_blank" rel="noopener">算法在线演示</a>
-            </li>
-        </ul>
-    </div>
+    <h3>小程序相关</h3>
+    <ul>
+      <li>
+        <a href="https://mp.weixin.qq.com/cgi-bin/wx" target="_blank" rel="noopener">官网</a>
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a href="https://uniapp.dcloud.io/" target="_blank" rel="noopener">uniapp</a>
+      </li>
+      <li>
+        <a href="http://mpvue.com/" target="_blank" rel="noopener">mpvue</a>
+      </li>
+    </ul>
+    <h3>人工智能</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/machinelearningmindset/TensorFlow-Course"
+          target="_blank"
+          rel="noopener"
+        >TensorFlow新手教程</a>
+      </li>
+      <li>
+        <a
+          href="https://developers.google.com/machine-learning/crash-course/"
+          target="_blank"
+          rel="noopener"
+        >Google机器学习课程</a>
+      </li>
+    </ul>
+    <h3>娱乐&游戏</h3>
+    <ul>
+      <li>
+        <a href="https://arcade.makecode.com/#" target="_blank" rel="noopener">来把，做一个游戏</a>
+      </li>
+      <li>
+        <a href="http://tools.jb51.net/games/gangqin" target="_blank" rel="noopener">在线钢琴flash</a>
+      </li>
+      <li>
+        <a href="https://github.com/WarpPrism/AutoPiano" target="_blank" rel="noopener">在线钢琴h5</a>
+      </li>
+      <li>
+        <a href="http://loderunnerwebgame.com/game/" target="_blank" rel="noopener">挖金子</a>
+      </li>
+      <li>
+        <a href="https://zhuanlan.zhihu.com/p/23936161" target="_blank" rel="noopener">非一般的游戏</a>
+      </li>
+      <li>
+        <a href="https://dos.zczc.cz/" target="_blank" rel="noopener">DOS游戏-非常经典</a>
+      </li>
+      <li>
+        <a
+          href="https://www.foldnfly.com/#/1-1-1-1-1-1-1-1-2"
+          target="_blank"
+          rel="noopener"
+        >来教你折纸飞机</a>
+      </li>
+      <li>
+        <a href="https://luden.io/wtl/" target="_blank" rel="noopener">人工智能游戏</a>
+      </li>
+      <li>
+        <a href="http://nandgame.com/" target="_blank" rel="noopener">计算机组成游戏</a>
+      </li>
+    </ul>
+    <h3>知名博主</h3>
+    <ul>
+      <li>
+        <a href="http://www.ruanyifeng.com/home.html" target="_blank" rel="noopener">ruanyifeng</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/sdmg15/Best-websites-a-programmer-should-visit"
+          target="_blank"
+          rel="noopener"
+        >programer</a>
+      </li>
+    </ul>
+    <h3>比较有意思的一些文章</h3>
+    <ul>
+      <li>
+        <a
+          href="https://juejin.im/entry/587e09201b69e600584cd326#foundation"
+          target="_blank"
+          rel="noopener"
+        >码农必须书籍</a>
+      </li>
+      <li>
+        <a
+          href="https://learn.sparkfun.com/tutorials/how-to-read-a-schematic/all"
+          target="_blank"
+          rel="noopener"
+        >电路图符号解释</a>
+      </li>
+      <li>
+        <a
+          href="http://blog.coderhuo.tech/2017/10/12/Virtual_Memory_C_strings_proc/"
+          target="_blank"
+          rel="noopener"
+        >虚拟内存最佳讲解</a>
+      </li>
+      <li>
+        <a
+          href="https://www.forbes.com/sites/maddieberg/2019/02/19/the-greatest-investor-youve-never-heard-of-an-optometrist-who-beat-the-odds-to-become-a-billionaire/#753dab3a22e8"
+          target="_blank"
+          rel="noopener"
+        >阅读专利投资得21亿</a>
+      </li>
+      <li>
+        <a href="https://github.com/MRNIU/SimpleKernel" target="_blank" rel="noopener">可以玩玩操作系统</a>
+      </li>
+      <li>
+        <a href="https://github.com/cfenollosa/os-tutorial" target="_blank" rel="noopener">从零玩操作系统</a>
+      </li>
+      <li>
+        <a
+          href="https://hadrienj.github.io/posts/Deep-Learning-Book-Series-Introduction/"
+          target="_blank"
+          rel="noopener"
+        >线性代数入门</a>
+      </li>
+      <li>
+        <a
+          href="https://hackernoon.com/writing-a-reverse-proxy-in-just-one-line-with-go-c1edfa78c84b"
+          target="_blank"
+          rel="noopener"
+        >用go手撸一个反向代理服务器</a>
+      </li>
+      <li>
+        <a
+          href="https://www.interviewcake.com/data-structures-reference"
+          target="_blank"
+          rel="noopener"
+        >常见数据结构简介</a>
+      </li>
+    </ul>
+    <h3>烧脑</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/azl397985856/leetcode"
+          target="_blank"
+          rel="noopener"
+        >leetcode解题思路</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/MisterBooo/LeetCodeAnimation"
+          target="_blank"
+          rel="noopener"
+        >leetcode解题动画</a>
+      </li>
+      <li>
+        <a href="https://algorithm-visualizer.org" target="_blank" rel="noopener">算法在线演示</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'HelloWorld',
-        props: {
-            msg: String
-        }
-    }
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    h3 {
-        margin: 40px 0 0;
-    }
+h3 {
+  margin: 40px 0 0;
+}
 
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-    a {
-        color: #42b983;
-    }
+a {
+  color: #42b983;
+}
 </style>
